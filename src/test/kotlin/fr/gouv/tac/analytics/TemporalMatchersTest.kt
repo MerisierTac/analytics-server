d09709557d1522c8db449da35c5893598b834b8c
Jujube Orange <13631-x-JOrang@users.noreply.gitlab.inria.fr>
2022-01-04 09:50:53 +0100

    tests: refactor units tests on temporal-matchers

--- src/test/kotlin/fr/gouv/tac/analytics/TemporalMatchersTest.kt
+++ src/test/kotlin/fr/gouv/tac/analytics/TemporalMatchersTest.kt
@@ -2,33 +2,56 @@ package fr.gouv.tac.analytics
 
 import fr.gouv.tac.analytics.test.TemporalMatchers.isStringDateBetweenNowAndTenSecondsAgo
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.not
-import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.DynamicTest.dynamicTest
-import org.junit.jupiter.api.TestFactory
-import java.time.ZonedDateTime.now
-import java.time.temporal.ChronoUnit.SECONDS
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.time.ZonedDateTime
 
 class TemporalMatchersTest {
 
-    private val now = now()
+    companion object {
+        @JvmStatic
+        fun timesNearNow() = listOf(
+            Arguments.arguments({ ZonedDateTime.now() }),
+            Arguments.arguments({ ZonedDateTime.now().minusSeconds(5) }),
+            Arguments.arguments({ ZonedDateTime.now().minusSeconds(9) })
+        )
 
-    @TestFactory
-    fun returns_true_if_result_is_between_now_and_ten_seconds_ago(): Iterable<DynamicTest> {
-        val tenSecondsAgo = now.minus(10, SECONDS)
-        return List(30) { now.minusSeconds(it.toLong()) }
-            .map { date ->
-                if (date.isAfter(tenSecondsAgo).or(date.isEqual(tenSecondsAgo))
-                    .and(date.isBefore(now).or(date.isEqual(now)))
-                ) {
-                    dynamicTest("$date matches because between $now and 10 seconds before") {
-                        assertThat(date.toString(), isStringDateBetweenNowAndTenSecondsAgo())
-                    }
-                } else {
-                    dynamicTest("$date does not match because more than 10 seconds before $now") {
-                        assertThat(date.toString(), not(isStringDateBetweenNowAndTenSecondsAgo()))
-                    }
-                }
-            }
+        @JvmStatic
+        fun timesMoreThanTenSecondsAgo() = listOf(
+            Arguments.arguments({ ZonedDateTime.now().plusSeconds(10) }),
+            Arguments.arguments({ ZonedDateTime.now().plusSeconds(15) }),
+            Arguments.arguments({ ZonedDateTime.now().plusSeconds(19) })
+        )
+
+        @JvmStatic
+        fun timesInFuture() = listOf(
+            Arguments.arguments({ ZonedDateTime.now().plusSeconds(1) }),
+            Arguments.arguments({ ZonedDateTime.now().plusSeconds(5) }),
+            Arguments.arguments({ ZonedDateTime.now().plusSeconds(9) })
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("timesNearNow")
+    fun can_verify_a_date_is_near_now_and_ten_seconds_ago(someTime: () -> ZonedDateTime) {
+        assertThat(someTime().toString(), isStringDateBetweenNowAndTenSecondsAgo())
+    }
+
+    @ParameterizedTest
+    @MethodSource("timesMoreThanTenSecondsAgo")
+    fun can_detect_a_date_is_more_than_ten_seconds_ago(someTime: () -> ZonedDateTime) {
+        assertThrows<AssertionError> {
+            assertThat(someTime().toString(), isStringDateBetweenNowAndTenSecondsAgo())
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("timesInFuture")
+    fun can_detect_a_date_is_in_the_future(someTime: () -> ZonedDateTime) {
+        assertThrows<AssertionError> {
+            assertThat(someTime().toString(), isStringDateBetweenNowAndTenSecondsAgo())
+        }
     }
 }
